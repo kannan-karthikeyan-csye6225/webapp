38281cebf28aba674440ef5ee4b12bd50a621f7b
Kannan Karthikeyan <kannan060200@hotmail.com>
2024-09-26 14:01:10 -0400

    Update healthController.js
    All the checks will be run only if the method is a GET, else it will directly throw a 405 method not allowed

--- src/routes/healthRoute.js
+++ src/routes/healthRoute.js
@@ -1,9 +1,8 @@
 import express from 'express';
-import { checkHealth, methodNotAllowed } from '../controllers/healthController.js';
+import { checkHealth } from '../controllers/healthController.js';
 
 const router = express.Router();
 
-router.get('/', checkHealth);
-router.all('/', methodNotAllowed);
+router.all('/', checkHealth);
 
 export default router;
