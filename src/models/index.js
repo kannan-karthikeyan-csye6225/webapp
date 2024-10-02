89ca33059483e0bfad1470d381f23e6baf61181e
Kannan Karthikeyan <kannan060200@hotmail.com>
2024-10-02 11:28:31 -0400

    Implement Bcrypt
    Bcrypt password hashing has been done using Sequelize hooks

--- src/models/index.js
+++ src/models/index.js
@@ -1,9 +1,10 @@
 import { Sequelize, DataTypes } from 'sequelize';
+import bcrypt from 'bcrypt';
 import sequelize from '../config/database.js';
 import logger from '../config/logger.js';
 
 // Sync function creates a Table if a table doesn't exist previously. Does nothing if the table exists already
-sequelize.sync({ force: false })  // Setting force to 'true' will drop the table and create a fresh one.
+sequelize.sync({ force: true })  // Setting force to 'true' will drop the table and create a fresh one.
   .then(() => {
     logger.info('Postgres Database Created (Synced)');
   })
@@ -38,5 +39,17 @@ const User = sequelize.define(
     updatedAt: 'account_updated'
   }
 );
+
+User.beforeCreate(async (user) => {
+  const saltRounds = 10; 
+  user.password = await bcrypt.hash(user.password, saltRounds);
+});
+
+User.beforeUpdate(async (user) => {
+  const saltRounds = 10;
+  if (user.changed('password')) {
+    user.password = await bcrypt.hash(user.password, saltRounds); 
+  }
+});
 
 export default User;
